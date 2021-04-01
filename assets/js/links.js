b48f18280883b5fcd6aca725e87215bdfe4bbade
nxj1129 <niko.jugovac@gmail.com>
2021-04-01 16:11:25 +0200

    Added another project to work page

--- assets/js/links.js
+++ assets/js/links.js
@@ -3,5 +3,7 @@ document.addEventListener("click", function (e) {
     window.open("https://reactsocialmedia-cbeed.firebaseapp.com/");
   } else if (e.target.id == "ski") {
     window.open("http://ski-retreat.herokuapp.com/");
+  } else if (e.target.id == "air") {
+    window.open("https://airbnb-clone-3f1c0.web.app/");
   }
 });
